e9ff97d9c7ff0130ad0edabcd30576acb3a6daa8
digitaltrails <michael@actrix.gen.nz>
2021-08-24 13:21:16 +1200

    Refactored for readability

--- ddc_controls.py
+++ ddc_controls.py
@@ -4,8 +4,10 @@
 # ddc_controls.py
 # Display Data Channel (DDC) - Virtual Control Panel (VCP)
 #
-# A GUI for retrieving and altering settings of connected monitors (via
-# ddcutil) by issuing DDC commands over HDMI/DVI/USB.
+# A GUI for retrieving and altering settings of connected VDU's (via
+# ddcutil) by issuing DDC commands over HDMI/DVI/USB.  This code
+# refers sometimes refers to displays and monitors as VDU's in order
+# to disambiguate the noun/verb duality of "display" and "monitor"
 #
 # Copyright (C) 2021 Michael Hamilton
 # This program is free software: you can redistribute it and/or modify it
@@ -41,18 +43,21 @@ import base64
 import traceback
 import argparse
 import signal
-#from enum import StrEnum
+# from enum import StrEnum
 
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QSlider, QMessageBox, QLineEdit, QLabel, QSplashScreen, QPushButton, QProgressBar
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QSlider, QMessageBox, QLineEdit, QLabel, \
+    QSplashScreen, QPushButton, QProgressBar
 from PyQt5.QtCore import Qt, QCoreApplication, QThread, pyqtSignal
 from PyQt5.QtGui import QIntValidator, QPixmap, QIcon
 from PyQt5.QtSvg import QSvgWidget
 
+
 def tr(source_text):
     return QCoreApplication.translate('ddc-control', source_text)
 
-DEFAULT_SPLASH_PNG="/usr/share/icons/oxygen/base/256x256/apps/preferences-desktop-display.png"
-FALLBACK_SPLASH_JPEG_BASE64=b"""
+
+DEFAULT_SPLASH_PNG = "/usr/share/icons/oxygen/base/256x256/apps/preferences-desktop-display.png"
+FALLBACK_SPLASH_JPEG_BASE64 = b"""
 /9j/4AAQSkZJRgABAQIARgBGAAD/2wBDAFA3PEY8MlBGQUZaVVBfeMiCeG5uePWvuZHI////////
 ////////////////////////////////////////////2wBDAVVaWnhpeOuCguv/////////////
 ////////////////////////////////////////////////////////////wAARCABAAEADASIA
@@ -122,65 +127,70 @@ VOLUME_SVG = b"""
 </svg>
 """
 
+DDCUTIL = "/usr/bin/ddcutil"
 
-DDCUTIL="/usr/bin/ddcutil"
-
-#class VcpType(StrEnum):
+# class VcpType(StrEnum):
 #    CONTINUOUS = 'C'
 #    SIMPLE_NON_CONTINUOUS = 'SNC'
 #    COMPLEX_NON_CONTINUOUS = 'CNC'
 
-class VcpCapability():
+
+class VcpCapability:
     """
-    Virtual Control Panel Command for monitors
+    Representation of a VCP (Virtual Control Panel) capability for a VDU.
     """
-    def __init__(self, code, name, icon=None, values={}):
+
+    def __init__(self, code, name, icon_source=None, values={}):
         self.name = name
         self.vcp_code = code
-        self.icon = icon
+        self.icon_source = icon_source
+        # For future use if we want to implement non-continuous types of VCP (VCP types SNC or CNC)
         self.values = values
 
     def arg_name(self):
         return self.name.replace(' ', '-').lower()
 
-# VCP commands to be made available as Qt sliders
-GUI_VCP_COMMANDS = [
-    VcpCapability('10', 'Brightness',   icon=BRIGHTNESS_SVG),
-    VcpCapability('12', 'Contrast',     icon=CONTRAST_SVG),
-    VcpCapability('62', 'Audio volume', icon=VOLUME_SVG),
-    ]
 
-class DdcUtil():
+# VCP capabilities to be made available as Qt sliders.
+# Any continuous (VCP type C) value can be supported.
+SUPPORTED_VCP_CAPABILITIES = [
+    VcpCapability('10', 'Brightness', icon_source=BRIGHTNESS_SVG),
+    VcpCapability('12', 'Contrast', icon_source=CONTRAST_SVG),
+    VcpCapability('62', 'Audio volume', icon_source=VOLUME_SVG),
+]
+
+
+class DdcUtil:
     """
-    Display Data Channel Utility for interacting with monitors
+    Interface to the command line ddcutil Display Data Channel Utility for interacting with VDU's.
     """
     def __init__(self, debug=False):
         super().__init__()
         self.debug = debug
 
-    def __run__(self, args):
+    def __run__(self, *args):
         if self.debug:
-            print("DEBUG: subprocess run    - ", args)
-        result = subprocess.run(args, stdout=subprocess.PIPE, check=True)
+            print("DEBUG: subprocess run    - ", DDCUTIL, args)
+        result = subprocess.run([DDCUTIL, ] + list(args), stdout=subprocess.PIPE, check=True)
         if self.debug:
             print("DEBUG: subprocess result - ", result)
         return result
 
-    def detect(self):
+    def detect_monitors(self):
         display_list = []
-        result = self.__run__([DDCUTIL, 'detect', '--terse'])
+        result = self.__run__('detect', '--terse')
         model_pattern = re.compile('Monitor:[ \t]+([^\n]*)')
-        for monitor_str in re.split("^Display |\nDisplay", result.stdout.decode('utf-8'))[1:]:
-            ddc_id = monitor_str.split('\n')[0].strip()
-            model_match = model_pattern.search(monitor_str)
-            model = model_match.group(1) if model_match else 'Unknown model'
-            display_list.append((ddc_id, model))
+        for display_str in re.split("^Display |\nDisplay", result.stdout.decode('utf-8'))[1:]:
+            ddc_id = display_str.split('\n')[0].strip()
+            model_match = model_pattern.search(display_str)
+            model_name = model_match.group(1) if model_match else 'Unknown model'
+            display_list.append((ddc_id, model_name))
         return display_list
 
     def query_capabilities(self, ddc_id):
         feature_pattern = re.compile(r'([0-9A-F]{2})\s+[(]([^)]+)[)]\n(Values:\n)?(.*)?', re.DOTALL)
         feature_map = {}
-        result = self.__run__([DDCUTIL, '--display', ddc_id, 'capabilities'])
+        result = self.__run__('--display', ddc_id, 'capabilities')
         for feature_text in result.stdout.decode('utf-8').split(' Feature: '):
             feature_match = feature_pattern.match(feature_text)
             if feature_match:
@@ -193,120 +203,201 @@ class DdcUtil():
         return feature_map
 
     def get_attribute(self, ddc_id, vcp_code):
-        result = self.__run__([DDCUTIL, '--brief', '--display', ddc_id, 'getvcp', vcp_code ])
+        result = self.__run__('--brief', '--display', ddc_id, 'getvcp', vcp_code)
         items = result.stdout.decode('utf-8').split()
-        return int(items[3]),int(items[4])
+        return int(items[3]), int(items[4])
 
     def set_attribute(self, ddc_id, vcp_code, new_value):
         current, _ = self.get_attribute(ddc_id, vcp_code)
         if new_value != current:
-            self.__run__([DDCUTIL, '--display', ddc_id, 'setvcp', vcp_code, str(new_value) ])
+            self.__run__('--display', ddc_id, 'setvcp', vcp_code, str(new_value))
 
 
 class DdcSliderWidget(QWidget):
     """
     Slider widget for DDC continuously variable attributes
     """
-    def __init__(self, ddcutil, monitor_id, vcp_command):
+
+    def __init__(self, ddcutil, monitor_id, vcp_capability):
         super().__init__()
 
         self.ddcutil = ddcutil
         self.monitor_id = monitor_id
-        self.vcp_command = vcp_command
-        self.current, self.maximum = ddcutil.get_attribute(monitor_id, vcp_command.vcp_code)
+        self.vcp_capability = vcp_capability
+        self.current_value, self.max_value = ddcutil.get_attribute(monitor_id, vcp_capability.vcp_code)
 
         layout = QHBoxLayout()
         self.setLayout(layout)
 
         icon = QSvgWidget()
-        icon.load(vcp_command.icon)
+        icon.load(vcp_capability.icon_source)
         icon.setFixedSize(50, 50)
-        icon.setToolTip(tr(vcp_command.name))
+        icon.setToolTip(tr(vcp_capability.name))
         layout.addWidget(icon)
 
         slider = QSlider()
         slider.setMinimumWidth(200)
-        slider.setValue(self.current)
-        slider.setRange(0, self.maximum)
+        slider.setValue(self.current_value)
+        slider.setRange(0, self.max_value)
         slider.setMinimum(0)
         slider.setSingleStep(1)
         slider.setPageStep(10)
         slider.setTickInterval(10)
         slider.setTickPosition(QSlider.TicksBelow)
         slider.setOrientation(Qt.Horizontal)
-        # Don't ewrite the ddc value too often - not sure of the implications
+        # Don't rewrite the ddc value too often - not sure of the implications
         slider.setTracking(False)
         layout.addWidget(slider)
         self.slider = slider
 
-        textinput = QLineEdit()
-        textinput.setMaximumWidth(50)
-        textinput.setMaxLength(4)
-        textvalidator = QIntValidator()
-        textvalidator.setRange(0, self.maximum)
-        textinput.setValidator(textvalidator)
-        textinput.setText(str(slider.value()))
-        layout.addWidget(textinput)
+        text_input = QLineEdit()
+        text_input.setMaximumWidth(50)
+        text_input.setMaxLength(4)
+        text_validator = QIntValidator()
+        text_validator.setRange(0, self.max_value)
+        text_input.setValidator(text_validator)
+        text_input.setText(str(slider.value()))
+        layout.addWidget(text_input)
 
         def slider_changed(value):
-            self.current = value
-            textinput.setText(str(value))
-            self.ddcutil.set_attribute(self.monitor_id, self.vcp_command.vcp_code, value)
+            self.current_value = value
+            text_input.setText(str(value))
+            self.ddcutil.set_attribute(self.monitor_id, self.vcp_capability.vcp_code, value)
+
         slider.valueChanged.connect(slider_changed)
 
         def slider_moved(value):
-            textinput.setText(str(value))
+            text_input.setText(str(value))
+
         slider.sliderMoved.connect(slider_moved)
 
         def text_changed():
-            slider.setValue(int(textinput.text()))
-        textinput.editingFinished.connect(text_changed)
+            slider.setValue(int(text_input.text()))
+
+        text_input.editingFinished.connect(text_changed)
 
     def refresh_data(self):
-        self.current, _ = self.ddcutil.get_attribute(self.monitor_id, self.vcp_command.vcp_code)
+        self.current_value, _ = self.ddcutil.get_attribute(self.monitor_id, self.vcp_capability.vcp_code)
 
     def refresh_view(self):
-        self.slider.setValue(self.current)
+        self.slider.setValue(self.current_value)
 
 
-class DdcMonitorWidget(QWidget):
+class DdcVduWidget(QWidget):
     """
-    Widget to control one monitor.feature_match
+    Widget to control one VDU (monitor/display)
     """
-    def __init__(self, ddcutil, monitor_id, monitor_name, hide, warnings):
+
+    def __init__(self, ddcutil, vdu_id, vdu_name, hide, warnings):
         super().__init__()
         layout = QVBoxLayout()
         label = QLabel()
-        #label.setStyleSheet("font-weight: bold");
-        label.setText(tr('Monitor {}: {}').format(monitor_id, monitor_name))
+        # label.setStyleSheet("font-weight: bold");
+        label.setText(tr('Monitor {}: {}').format(vdu_id, vdu_name))
         layout.addWidget(label)
-        self.capabilities = ddcutil.query_capabilities(monitor_id)
-        self.controls = []
-        for vcp_command in GUI_VCP_COMMANDS:
-            if vcp_command.arg_name() not in hide:
-                if vcp_command.vcp_code in self.capabilities:
-                    control = DdcSliderWidget(ddcutil, monitor_id, vcp_command)
+        self.capabilities = ddcutil.query_capabilities(vdu_id)
+        self.vcp_controls = []
+        for capability in SUPPORTED_VCP_CAPABILITIES:
+            if capability.arg_name() not in hide:
+                if capability.vcp_code in self.capabilities:
+                    control = DdcSliderWidget(ddcutil, vdu_id, capability)
                     layout.addWidget(control)
-                    self.controls.append(control)
+                    self.vcp_controls.append(control)
                 elif warnings:
                     alert = QMessageBox()
-                    alert.setText(tr('Monitor {} lacks a VCP control for {}.').format(monitor_name, tr(vcp_command.name)))
-                    alert.setInformativeText(tr('No read/write ability for vcp_code {}.').format(vcp_command.vcp_code))
+                    alert.setText(
+                        tr('Monitor {} lacks a VCP control for {}.').format(vdu_name, tr(capability.name)))
+                    alert.setInformativeText(tr('No read/write ability for vcp_code {}.').format(capability.vcp_code))
                     alert.setIcon(QMessageBox.Warning)
                     alert.exec()
-        if len(self.controls) != 0:
+        if len(self.vcp_controls) != 0:
             self.setLayout(layout)
 
     def refresh_data(self):
-        for control in self.controls:
+        for control in self.vcp_controls:
             control.refresh_data()
 
     def refresh_view(self):
-        for control in self.controls:
+        for control in self.vcp_controls:
             control.refresh_view()
 
     def number_of_controls(self):
-        return len(self.controls)
+        return len(self.vcp_controls)
+
+
+class DdcMainWidget(QWidget):
+
+    def __init__(self, args, splash):
+        super().__init__()
+        layout = QVBoxLayout()
+
+        ddcutil = DdcUtil(debug=args.debug)
+        self.vdu_widgets = []
+        for vdu_id, desc in ddcutil.detect_monitors():
+            splash.showMessage(tr('DDC Control\nDDC ID {}\n{}').format(vdu_id, desc),
+                               Qt.AlignVCenter | Qt.AlignHCenter)
+            vdu_widget = DdcVduWidget(ddcutil, vdu_id, desc, args.hide, args.warnings)
+            if vdu_widget.number_of_controls() != 0:
+                self.vdu_widgets.append(vdu_widget)
+                layout.addWidget(vdu_widget)
+
+        if len(self.vdu_widgets) == 0:
+            alert = QMessageBox()
+            alert.setText(tr('No controllable monitors found, exiting.'))
+            alert.setInformativeText(tr(
+                '''Run ddc_control --debug in a console and check for additional messages.\
+                Check the requirements for the ddcutil command.'''))
+            alert.setIcon(QMessageBox.Critical)
+            alert.exec()
+            sys.exit()
+
+        def start_refresh():
+            self.refresh_button.setDisabled(True)
+            self.progressBar.setDisabled(False)
+            # Setting range to 0,0 cause the progress bar to pulsate - used as a busy spinner.
+            self.progressBar.setRange(0, 0)
+            # Start a background task
+            self.refreshDataTask.start()
+
+        def finish_refresh():
+            # GUI-thread QT signal handler for refresh task completion
+            # Stop the busy-spinner progress bar pulsation
+            self.progressBar.setRange(0, 1)
+            self.progressBar.setDisabled(True)
+            self.refresh_button.setDisabled(False)
+            for vw in self.vdu_widgets:
+                vw.refresh_view()
+
+        self.refreshDataTask = RefreshFromVduTask(self)
+        self.refreshDataTask.task_finished.connect(finish_refresh)
+
+        self.progressBar = QProgressBar(self)
+        # Disable text percentage label on the spinner progress-bar
+        self.progressBar.setTextVisible(False)
+        self.progressBar.setRange(0, 1)
+        self.progressBar.setDisabled(True)
+        layout.addWidget(self.progressBar, Qt.AlignVCenter)
+
+        self.refresh_button = QPushButton(tr("Refresh settings from monitors"))
+        self.refresh_button.clicked.connect(start_refresh)
+        layout.addWidget(self.refresh_button)
+
+        self.setLayout(layout)
+
+
+class RefreshFromVduTask(QThread):
+    task_finished = pyqtSignal()
+
+    def __init__(self, ddc_widget):
+        super().__init__()
+        self.ddc_widget = ddc_widget
+
+    def run(self):
+        # Running in a task thread, cannot interact with GUI thread, just update the data.
+        for vdu_widget in self.ddc_widget.vdu_widgets:
+            vdu_widget.refresh_data()
+        # Tell the GUI-thread the task has finished, the GUI thread will then update the view widgets.
+        self.task_finished.emit()
 
 
 def exception_handler(etype, evalue, etraceback):
@@ -318,81 +409,14 @@ def exception_handler(etype, evalue, etraceback):
     alert.exec()
     QApplication.quit()
 
-class DdcControlWidget(QWidget):
-
-    def __init__(self, args, splash):
-        super().__init__()
-        layout = QVBoxLayout()
-
-        ddcutil = DdcUtil(debug=args.debug)
-        self.monitor_widgets = []
-        for monitor_id, desc in ddcutil.detect():
-            splash.showMessage(tr('DDC Control\nDDC ID {}\n{}').format(monitor_id, desc), Qt.AlignVCenter|Qt.AlignHCenter)
-            monitor_widget = DdcMonitorWidget(ddcutil, monitor_id, desc, args.hide, args.warnings)
-            if monitor_widget.number_of_controls() != 0:
-                self.monitor_widgets.append(monitor_widget)
-                layout.addWidget(monitor_widget)
-
-        if len(self.monitor_widgets) == 0:
-            alert = QMessageBox()
-            alert.setText(tr('No controllable monitors found, exiting.'))
-            alert.setInformativeText(tr(
-                '''Run ddc_control in a console and check for additional messages.\
-                Check the requirements for the ddcutil command.'''))
-            alert.setIcon(QMessageBox.Critical)
-            alert.exec()
-            sys.exit()
-
-        self.refreshTask = RefreshTaskThread(self)
-        self.refreshTask.taskFinished.connect(self.onFinished)
-
-        self.progressBar = QProgressBar(self)
-        self.progressBar.setTextVisible(False)
-        self.progressBar.setRange(0,1)
-        self.progressBar.setDisabled(True)
-        layout.addWidget(self.progressBar, Qt.AlignVCenter|Qt.AlignHCenter)
-
-        self.refresh_button = QPushButton(tr("Refresh settings from monitors"))
-        self.refresh_button.clicked.connect(self.onStart)
-        layout.addWidget(self.refresh_button)
-
-        self.setLayout(layout)
-
-    def onStart(self):
-        self.refresh_button.setDisabled(True)
-        self.progressBar.setDisabled(False)
-        self.progressBar.setRange(0,0)
-        self.refreshTask.start()
-
-    def onFinished(self):
-        # Stop the pulsation
-        self.progressBar.setRange(0,1)
-        self.progressBar.setDisabled(True)
-        self.refresh_button.setDisabled(False)
-        for widget in self.monitor_widgets:
-            widget.refresh_view()
-
-class RefreshTaskThread(QThread):
-
-    taskFinished = pyqtSignal()
-
-    def __init__(self, ddc_widget):
-        super().__init__()
-        self.ddc_widget = ddc_widget
-
-    def run(self):
-        for widget in self.ddc_widget.monitor_widgets:
-            widget.refresh_data()
-        self.taskFinished.emit()
-
-
 
 def main():
     # Allow control-c to terminate the program
     signal.signal(signal.SIGINT, signal.SIG_DFL)
 
     parser = argparse.ArgumentParser(description='Display Data Channel - Virtual Control Panel')
-    parser.add_argument('--hide', default=[], action='append', choices=[ vcp.arg_name() for vcp in GUI_VCP_COMMANDS ], help='hide/disable a control')
+    parser.add_argument('--hide', default=[], action='append', choices=[vcp.arg_name() for vcp in SUPPORTED_VCP_CAPABILITIES],
+                        help='hide/disable a control')
     # Python 3.9 parser.add_argument('--debug',  action=argparse.BooleanOptionalAction, help='enable debugging')
     parser.add_argument('--debug', default=False, action='store_true', help='enable debugging')
     parser.add_argument('--warnings', default=False, action='store_true', help='enable missing feature warnings')
@@ -417,12 +441,13 @@ def main():
     app.setWindowIcon(app_icon)
     app.setApplicationDisplayName(tr('DDC Control'))
 
-    splash.showMessage(tr('DDC Control\nLooking for DDC monitors...\n'), Qt.AlignVCenter|Qt.AlignHCenter)
-    main_window = DdcControlWidget(args, splash)
+    splash.showMessage(tr('DDC Control\nLooking for DDC monitors...\n'), Qt.AlignVCenter | Qt.AlignHCenter)
+    main_window = DdcMainWidget(args, splash)
     main_window.show()
     splash.finish(main_window)
 
     sys.exit(app.exec_())
+
 
 if __name__ == '__main__':
     main()
