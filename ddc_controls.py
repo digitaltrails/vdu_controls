99a7e1b4a91e205e3e4cc9d136908e63bb1c4429
digitaltrails <michael@actrix.gen.nz>
2021-08-26 14:31:04 +1200

    Improved the splash screen.  Added some type hints. Improved/added doc comments.

--- ddc_controls.py
+++ ddc_controls.py
@@ -45,6 +45,7 @@ import time
 import traceback
 import argparse
 import signal
+from typing import List, Tuple, Mapping
 
 from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QSlider, QMessageBox, QLineEdit, QLabel, \
     QSplashScreen, QPushButton, QProgressBar
@@ -53,25 +54,10 @@ from PyQt5.QtGui import QIntValidator, QPixmap, QIcon
 from PyQt5.QtSvg import QSvgWidget
 
 
-def translate(source_text):
+def translate(source_text: str):
+    """For future internationalization - recommended way to do this at this time."""
     return QCoreApplication.translate('ddc-control', source_text)
 
-
-# Encode some default graphics to make the script self contained
-DEFAULT_SPLASH_PNG = "/usr/share/icons/oxygen/base/256x256/apps/preferences-desktop-display.png"
-FALLBACK_SPLASH_JPEG_BASE64 = b"""
-/9j/4AAQSkZJRgABAQIARgBGAAD/2wBDAFA3PEY8MlBGQUZaVVBfeMiCeG5uePWvuZHI////////
-////////////////////////////////////////////2wBDAVVaWnhpeOuCguv/////////////
-////////////////////////////////////////////////////////////wAARCABAAEADASIA
-AhEBAxEB/8QAGAAAAwEBAAAAAAAAAAAAAAAAAAIDBAH/xAAsEAACAQIDBgUFAQAAAAAAAAABAgAD
-ERJTcQQiM0Gx0SEyUZOiI2GRkuFD/8QAFgEBAQEAAAAAAAAAAAAAAAAAAAEC/8QAGBEBAQEBAQAA
-AAAAAAAAAAAAAAEREgL/2gAMAwEAAhEDEQA/AJolIUUZkLM1+do2GjlD94q8Gjqes00z9NdBLIlq
-GGjlD3IYKOUPc/s03heMNZsFHKHuf2GGjlD3JpvC8Yms2GjlD94rpSNF2VCrLbneaKhujaTO3Bra
-jrFmLLoHBo6nrNCHcXSZwfpUdT1lUO4NJfKVS8Lxbzl5rGT4py8W8Lxg6/kbSQPBrajrLN5G0kDw
-q2o6zPpryBwaOp6yindGkkrUzSQM+ErfleNipZvxPeSXFs1S8LxMVLM+J7x0CuLq1xoe811GeXYT
-jhUF2aw0PeJjp5vxPeOjk7eQ6SX+VbUdZ3FTzfie8Vmpik4V8Ra3K0zbrUmITqjEwUczachIremy
-U1HjvH1MqFwiy2A9BMSbVUUWNmH3lBto50/wYGg0w3m8R95N9kpsPDdPqJM7aOVP8mTfaqjCwso+
-0CLLhYr6G05CED//2Q=="""
 
 BRIGHTNESS_SVG = b"""
 <svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 24 24" width="24" height="24">
@@ -129,9 +115,22 @@ VOLUME_SVG = b"""
 </svg>
 """
 
+# Encode some default graphics to make the script self contained
+DEFAULT_SPLASH_PNG = "/usr/share/icons/oxygen/base/256x256/apps/preferences-desktop-display.png"
+
 DDCUTIL = "/usr/bin/ddcutil"
 
 EXIT_CODE_FOR_RESTART = 1959
+
+
+def get_splash_image() -> QPixmap:
+    """Gets the splash pixmap from a KDE oxygen PNG file or, failing that, a small base64 encoded internal JPEG."""
+    pixmap = QPixmap()
+    if os.path.isfile(DEFAULT_SPLASH_PNG) and os.access(DEFAULT_SPLASH_PNG, os.R_OK):
+        pixmap.load(DEFAULT_SPLASH_PNG)
+    else:
+        pixmap.loadFromData(base64.decodebytes(FALLBACK_SPLASH_JPEG_BASE64), 'JPEG')
+    return pixmap
 
 
 # class VcpType(StrEnum):
@@ -141,18 +140,15 @@ EXIT_CODE_FOR_RESTART = 1959
 
 
 class VcpCapability:
-    """
-    Representation of a VCP (Virtual Control Panel) capability for a VDU.
-    """
-
-    def __init__(self, code, name, icon_source=None, values=None):
+    """ Representation of a VCP (Virtual Control Panel) capability for a VDU. """
+    def __init__(self, vcp_code: str, name: str, icon_source: bytes = None, values: List[str] = None):
+        self.vcp_code = vcp_code
         self.name = name
-        self.vcp_code = code
         self.icon_source = icon_source
         # For future use if we want to implement non-continuous types of VCP (VCP types SNC or CNC)
         self.values = [] if values is None else values
 
-    def arg_name(self):
+    def arg_name(self) -> str:
         return self.name.replace(' ', '-').lower()
 
 
@@ -166,16 +162,13 @@ SUPPORTED_VCP_CAPABILITIES = [
 
 
 class DdcUtil:
-    """
-    Interface to the command line ddcutil Display Data Channel Utility for interacting with VDU's.
-    """
-
-    def __init__(self, debug=False, common_args=None):
+    """ Interface to the command line ddcutil Display Data Channel Utility for interacting with VDU's. """
+    def __init__(self, debug: bool = False, common_args: List[str] = None):
         super().__init__()
         self.debug = debug
         self.common_args = [] if common_args is None else common_args
 
-    def __run__(self, *args):
+    def __run__(self, *args) -> subprocess.CompletedProcess:
         if self.debug:
             print("DEBUG: subprocess run    - ", DDCUTIL, args)
         result = subprocess.run([DDCUTIL, ] + self.common_args + list(args), stdout=subprocess.PIPE, check=True)
@@ -183,7 +176,8 @@ class DdcUtil:
             print("DEBUG: subprocess result - ", result)
         return result
 
-    def detect_monitors(self):
+    def detect_monitors(self) -> List[Tuple[str, str]]:
+        """ Returns a list of (vdu_id, desc) tuples """
         display_list = []
         result = self.__run__('detect', '--terse')
         model_pattern = re.compile('Monitor:[ \t]+([^\n]*)')
@@ -194,9 +188,10 @@ class DdcUtil:
             display_list.append((ddc_id, model_name))
         return display_list
 
-    def query_capabilities(self, ddc_id):
+    def query_capabilities(self, ddc_id: str) -> Mapping[str, VcpCapability]:
+        """Returns a map of vpc capabilities keyed by vcp code."""
         feature_pattern = re.compile(r'([0-9A-F]{2})\s+[(]([^)]+)[)]\n(Values:\n)?(.*)?', re.DOTALL)
-        feature_map = {}
+        feature_map: Mapping[str, VcpCapability] = {}
         result = self.__run__('--display', ddc_id, 'capabilities')
         for feature_text in result.stdout.decode('utf-8').split(' Feature: '):
             feature_match = feature_pattern.match(feature_text)
@@ -209,7 +204,8 @@ class DdcUtil:
             print("DEBUG: capabilities", feature_map.keys())
         return feature_map
 
-    def get_attribute(self, ddc_id, vcp_code):
+    def get_attribute(self, ddc_id: str, vcp_code: str) -> Tuple[int, int]:
+        """ Given a VDU id and vcp_code, returns the attributes current and maximum value. """
         value_pattern = re.compile(r'VCP ' + vcp_code + r' [A-Z]+ ([0-9]+) ([0-9]+)\n')
         # Try a few times in case there is a glitch due to a monitor being turned off/on
         for i in range(3):
@@ -224,18 +220,15 @@ class DdcUtil:
                 return int(value_match.group(1)), int(value_match.group(2))
         return 0, 0
 
-    def set_attribute(self, ddc_id, vcp_code, new_value):
+    def set_attribute(self, ddc_id: str, vcp_code: str, new_value: str):
         current, _ = self.get_attribute(ddc_id, vcp_code)
         if new_value != current:
             self.__run__('--display', ddc_id, 'setvcp', vcp_code, str(new_value))
 
 
 class DdcSliderWidget(QWidget):
-    """
-    Slider widget for DDC continuously variable attributes
-    """
-
-    def __init__(self, ddcutil, monitor_id, vcp_capability):
+    """ GUI control for a DDC continuously variable attribute. Compound widget with icon, slider and text field."""
+    def __init__(self, ddcutil: DdcUtil, monitor_id: str, vcp_capability: VcpCapability):
         super().__init__()
 
         self.ddcutil = ddcutil
@@ -252,7 +245,7 @@ class DdcSliderWidget(QWidget):
         icon.setToolTip(translate(vcp_capability.name))
         layout.addWidget(icon)
 
-        slider = QSlider()
+        self.slider = slider = QSlider()
         slider.setMinimumWidth(200)
         slider.setValue(self.current_value)
         slider.setRange(0, self.max_value)
@@ -265,7 +258,6 @@ class DdcSliderWidget(QWidget):
         # Don't rewrite the ddc value too often - not sure of the implications
         slider.setTracking(False)
         layout.addWidget(slider)
-        self.slider = slider
 
         text_input = QLineEdit()
         text_input.setMaximumWidth(50)
@@ -294,18 +286,18 @@ class DdcSliderWidget(QWidget):
         text_input.editingFinished.connect(text_changed)
 
     def refresh_data(self):
+        """Query the VDU for a new data value and cache it (may be called from a task thread, so no GUI op's here.)"""
         self.current_value, _ = self.ddcutil.get_attribute(self.monitor_id, self.vcp_capability.vcp_code)
 
     def refresh_view(self):
+        """Copy the internally cached current value onto the GUI view."""
         self.slider.setValue(self.current_value)
 
 
 class DdcVduWidget(QWidget):
-    """
-    Widget to control one VDU (monitor/display)
-    """
-
-    def __init__(self, ddcutil, vdu_id, vdu_desc, enabled_capabilities, warnings):
+    """ Widget that contains all the controls for a single VDU (monitor/display) """
+    def __init__(self, ddcutil: DdcUtil, vdu_id: str, vdu_desc: str, enabled_capabilities: List[VcpCapability],
+                 warnings: bool):
         super().__init__()
         layout = QVBoxLayout()
         label = QLabel()
@@ -333,35 +325,44 @@ class DdcVduWidget(QWidget):
             self.setLayout(layout)
 
     def refresh_data(self):
+        """Tell the control widgets to get fresh VDU data (may be called from a task thread, so no GUI op's here.)"""
         for control in self.vcp_controls:
             control.refresh_data()
 
     def refresh_view(self):
+        """Tell the control widgets to refresh their views from their internally cached values."""
         for control in self.vcp_controls:
             control.refresh_view()
 
-    def number_of_controls(self):
+    def number_of_controls(self) -> int:
+        """Return the number of VDU controls.  Might be zero if initialization discovered no controllable attributes."""
         return len(self.vcp_controls)
 
 
 class DdcMainWidget(QWidget):
-
-    def __init__(self, enabled_capabilities, warnings, debug, sleep_multiplier, splash):
+    """ Detects connected VDU's and constructs a window containing a control panel each that is found. """
+    def __init__(self, enabled_capabilities: List[VcpCapability], warnings: bool, debug: bool, sleep_multiplier: float,
+                 detect_vdu_hook: callable):
         super().__init__()
         layout = QVBoxLayout()
-        print(sleep_multiplier)
         self.ddcutil = DdcUtil(debug=debug, common_args=['--sleep-multiplier', str(sleep_multiplier)])
         self.vdu_widgets = []
         self.enabled_capabilities = enabled_capabilities
         self.warnings = warnings
         self.detected_vdus = self.ddcutil.detect_monitors()
         for vdu_id, desc in self.detected_vdus:
-            splash.showMessage(translate('DDC Control\nDDC ID {}\n{}').format(vdu_id, desc),
-                               Qt.AlignVCenter | Qt.AlignHCenter)
+            if detect_vdu_hook is not None:
+                detect_vdu_hook(vdu_id, desc)
             vdu_widget = DdcVduWidget(self.ddcutil, vdu_id, desc, enabled_capabilities, warnings)
             if vdu_widget.number_of_controls() != 0:
                 self.vdu_widgets.append(vdu_widget)
                 layout.addWidget(vdu_widget)
+            elif warnings:
+                alert = QMessageBox()
+                alert.setText(translate('Monitor {} {} lacks any accessible controls.').format(vdu_id, desc))
+                alert.setInformativeText(translate('The monitor will be omitted from the control panel.'))
+                alert.setIcon(QMessageBox.Warning)
+                alert.exec()
 
         if len(self.vdu_widgets) == 0:
             alert = QMessageBox()
@@ -406,12 +407,14 @@ class DdcMainWidget(QWidget):
         self.setLayout(layout)
 
     def refresh_data(self):
+        """Called by a non-GUI task to obtain new data from all monitors.  The task thread cannot do any GUI op's"""
         self.detected_vdus = self.ddcutil.detect_monitors()
         for vdu_widget in self.vdu_widgets:
             if (vdu_widget.vdu_id, vdu_widget.vdu_desc) in self.detected_vdus:
                 vdu_widget.refresh_data()
 
     def refresh_view(self):
+        """Callback when the GUI worker thread completes, the GUI thread and can refresh the GUI views."""
         to_do = self.detected_vdus.copy()
         for vdu_widget in self.vdu_widgets:
             if (vdu_widget.vdu_id, vdu_widget.vdu_desc) in to_do:
@@ -427,6 +430,11 @@ class DdcMainWidget(QWidget):
 
 
 class RefreshFromVduTask(QThread):
+    """
+    Task to refresh VDU data from the physical VDU's.  Run as a task because
+    it can be quite slow depending on the number of VDU's, number of controls,
+    as well as ddcutil parameters such as the sleep-multiplier.
+    """
     task_finished = pyqtSignal()
 
     def __init__(self, ddc_widget):
@@ -466,6 +474,7 @@ def main():
     # Python 3.9 parser.add_argument('--debug',  action=argparse.BooleanOptionalAction, help='enable debugging')
     parser.add_argument('--debug', default=False, action='store_true', help='enable debugging')
     parser.add_argument('--warnings', default=False, action='store_true', help='enable missing feature warnings')
+    parser.add_argument('--no-splash', default=False, action='store_true', help="don't show the splash screen")
     parser.add_argument('--sleep-multiplier', type=float, default="0.5", help='ddcutil sleep multiplier (>0.0)')
 
     args = parser.parse_args()
@@ -474,15 +483,13 @@ def main():
 
     app = QApplication(sys.argv)
 
-    pixmap = QPixmap()
-    if os.path.isfile(DEFAULT_SPLASH_PNG) and os.access(DEFAULT_SPLASH_PNG, os.R_OK):
-        pixmap.load(DEFAULT_SPLASH_PNG)
-    else:
-        pixmap.loadFromData(base64.decodebytes(FALLBACK_SPLASH_JPEG_BASE64), 'JPEG')
+    pixmap = get_splash_image()
 
-    splash = QSplashScreen(pixmap.scaledToWidth(800).scaledToHeight(400))
+    splash = None if args.no_splash else QSplashScreen(pixmap.scaledToWidth(800).scaledToHeight(400),
+                                                       Qt.WindowStaysOnTopHint)
 
-    splash.show()
+    if splash is not None:
+        splash.show()
 
     app_icon = QIcon()
     app_icon.addPixmap(pixmap)
@@ -494,16 +501,64 @@ def main():
     else:
         enabled_capabilities = [c for c in SUPPORTED_VCP_CAPABILITIES if c.arg_name() not in args.hide]
 
-    splash.showMessage(translate('DDC Control\nLooking for DDC monitors...\n'), Qt.AlignVCenter | Qt.AlignHCenter)
-    main_window = DdcMainWidget(enabled_capabilities, args.warnings, args.debug, args.sleep_multiplier, splash)
+    if splash is not None:
+        splash.showMessage(translate('\n\nDDC Control\nLooking for DDC monitors...\n'), Qt.AlignTop | Qt.AlignHCenter)
+
+    def detect_vdu_hook(vdu_id, desc):
+        if splash is not None:
+            splash.showMessage(translate('\n\nDDC Control\nDDC ID {}\n{}').format(vdu_id, desc),
+                               Qt.AlignTop | Qt.AlignHCenter)
+
+    main_window = DdcMainWidget(enabled_capabilities, args.warnings, args.debug, args.sleep_multiplier, detect_vdu_hook)
     main_window.show()
-    splash.finish(main_window)
+    if splash is not None:
+        splash.finish(main_window)
 
     rc = app.exec_()
     if rc == EXIT_CODE_FOR_RESTART:
         QProcess.startDetached(app.arguments()[0], app.arguments()[1:])
     sys.exit(rc)
 
+
+FALLBACK_SPLASH_JPEG_BASE64 = b"""
+/9j/4AAQSkZJRgABAQAAAQABAAD/2wBDACAWGBwYFCAcGhwkIiAmMFA0MCwsMGJGSjpQdGZ6eHJmcG6AkLicgIiuim5woNqirr7EztDOfJri8uDI8LjKzs
+b/2wBDASIkJDAqMF40NF7GhHCExsbGxsbGxsbGxsbGxsbGxsbGxsbGxsbGxsbGxsbGxsbGxsbGxsbGxsbGxsbGxsbGxsb/wgARCAEAAQADASIAAhEBAxEB
+/8QAGQABAQEBAQEAAAAAAAAAAAAAAAEEAgMF/8QAFwEBAQEBAAAAAAAAAAAAAAAAAAIBA//aAAwDAQACEAMQAAAB+eAAAAAAAAAAAAAAAADuzbs5G5uYW4
+YW0YrtGK7BkuoZmkZ+dUMTaMbYMbYMbYMfOrLleYzQO9uLbsdi4AAAFIpsoQMAAADXll1Zo6eQmgO9mTXUegqAAFAABLANAAACHnm05o6eQmgPTXk17Pdl
+vmAoBoMCaEKBYYDSAQ88+jNHTzE0B6a8mup9BXMNVAAAlABG5UNAICMeefRnjp5iaA9NeTXUeiW4AIAFgDQhUFiFQCNsDz8Pfw535iaA9NeTXUdi4EKgqU
+Q0AQWIVAI2oAbx4e/hzrzE0B66c2mo9EnSKjFRqoBCoABCoaAAQ3nx9fHnXmJoD01ZdNR3C4qNVBYhUFQENqAAAhtQc+Pr4864E0B6XmsrkdubrqODpwO5
+yx2nWhyHDHbgduGu3A7cDuS44DQFgqCoKgqCoPo+WjsxafQfJQVBUFQVBYAAAAAB66DE2jE2jW4h24HzuN3Bkaxka/A8wAAAAAAD2NvtyO3Bl8+43h1GQD
+vgdOYdefUPnPXyaAAAAAA+h8/1N9wjcwjcw02MY2TINTLDVMw0zOPXH6+QAAAAAAAAAAAAAAAAAAB//8QAJxAAAQIFBAMAAgMAAAAAAAAAAAECESAhMkID
+EhNAIjAxFGAQQVD/2gAIAQEAAQUC/QkolSpUqVKlSpWaBAUqVKlSpUqVKi/JMWW9h9uMmLLexqW4yYst7GpbhJgy3saluEmDLexqW4SYMt7D7cJMGW9h9u
+EmDLew+3CTBlvYfbhJgy3sPtwkwZb2H/MJMGW9h/zCTBtvYf8AMJMG29h/zCTBtvYd8wkxb87DvmEmLfnYd8xkxipFSKkVIqRUipuU3KRU3KeSnkeR5G5T
+cpuU3KblNym5TcpuU3KRMZE+etFgbjdSPs/qWKkVIqRUipFSKkVIqRUio1vjqoppou7ahFSKkVIqRUipFSKkVIqRXpMs/wAN1/VYzev45+OfjnAJpIkrtN
+FOA4DhOEe3avQ027W/ypD1PTc336TYu9MJ3pBfcxIN9VSpUqVKjki33JqOQ5XnK85nnM85nnM85nnM85nnM85nnM85nnM85XnK8XUcv6R//8QAHBEAAQUB
+AQEAAAAAAAAAAAAAAAECEBIwEUBg/9oACAEDAQE/AfCq8LFixYsWLFixYsWEXsu1bLtWy7Vsu1bLtUl2qS7VJdqmHPmv/8QAHhEAAgIDAQADAAAAAAAAAA
+AAABECEAESMDEgQGD/2gAIAQIBAT8B+jgQhCNRCEampqambx1leOsrx1lcesrj1lcesrj1l7cesvfkxjHTGMY/yf8A/8QAJBAAAgICAgEEAwEAAAAAAAAA
+ADEBcUChIDIREDBg4RIhUIH/2gAIAQEABj8C+BeRaFoWhaFoWhaFoWhaFoWj6Po+vRaFoWhaFoWhaFoWhaPPKM+CMr/eMWRlTfGLIypvjFkZU3xiyMqb4x
+eXN8YvLm+MWRlTfGLy5vjF5c3xi8ub4xeXN8YvLm+MXlzfGLy5v5MxjGMYxjGMgj8T9+fRjGMYxjGMeFFfxJvH7aOx2Ow/PHydjsdjt/Rr2Xjx7qEIQvfY
+xjGMYxjGMYxjGP4R/8QAKBAAAQIDCAIDAQEAAAAAAAAAAAERIWGRIDFAUXGxwfBBoRAwgWDh/9oACAEBAAE/If4JnkK7D5VJp0mnSaVJpUmlSadI+VSaFB
+HJQRyUH4oESSUDJklAzJKBmSUQSyQQq6DRoNCk0KDQpNCg0KDQoNCgfIoEovgW6z46rwelipb+qc2d5eMWhb+jmzvOMWhfQ5s7jjFoX0ObO44xYF9DmzuO
+MWJfQ5s7jguMVbDmzuOC4xVsObO44xYthzZ3HBcYo2HNnccFxinYc2dxwXGKdhzZT2OC6xTtObKexwXWKdpzZT2OC6xXtObKe5wXeK9hzZS7qvBc4r89U5
+spc1UY8k4nE4nEwXOJhMJxMEW4dR5z9itR1cmEwmEwmEwmEwmEwVS3qLe1T617UgJGkEgkCFDJHyoq1y+xYa1s3ExUmCYJgmCYJgmCYJgmBBVJCKAv4L1I
+ljefimCYJgmCYJgmCYJgmCYL8CqiHHHxrjjjnusMswisiefgdWOrCt/yR5VBxxxxXVyoqnRjox2Y7MKtXovnAteaxX4cciQct0SKeBx/j0XIOOOQzz4wET
+W6IcccccgKj51Gmo03wgg44445HfC/fd6+JHJSOSkclI5KRyUjkpHJSOS0I5LQjktBsi0GyLQbMoNmUGzKDZlBdSKEfvRkRIEOxDsQ6EOhDoQ6kO5CWoS1
+CWodSHUh0IdCHYh2IIyosCy/iP/aAAwDAQACAAMAAAAQAAAAAAAAAAAAAAAAAxICOCySfe+OOOEAArxBDf7ygjDDDN4AA1BDfjDPTxhM+mgAAGrXiBT1lG
+ryO2kAAUH8MxxyCNIwIv0AAdISu3uw0MUpkR1AAr2+PLYwIVJ0N8XAAXMPNp1N9lIzzuPAArTFNV9B8IxzuOPAAb/RhNf4hPPNNdTAAwwwwwwgQwwwwwwA
+AAAAAMEm+MMEAAAAAAAEyPejbPnjAAAAAAAEiOIySm6SIAAAAAAAAAAAAAAAAAAA/8QAHREAAgMBAQADAAAAAAAAAAAAETEAATAQIEBgcf/aAAgBAwEBPx
+D4Q/OkAAD1NU6mq9TA+U6mqdTVerqvV1Xqar4FQVBUFQIKgqCoKgqCoKlfUv/EABwRAAEFAQEBAAAAAAAAAAAAABEAARAxUTBAYP/aAAgBAgEBPxDwsPX3
+Bgeb9bdYm0362m3W0262m3W828FbwVIui6JFqLUWoui1FqLUWotTufkv/8QAKxAAAgEBBgUEAwEBAAAAAAAAAAER8BAgITFRYUGRobHxMHGB4UDB0WBQ/9
+oACAEBAAE/EP8ABIjFMZE1KUJY9RMUpyo4E0DsTQOxNI7E0jsTSOxNA7FEfoTrXYVe/QlRuwk6TsSZ1mw5sI6OAqM7CpbsKx4aM/RNS7E1jsTWuxNY7E1r
+sVv5Fb+RW/kNClpW/wDIgNJDTJKE5nHpdptgqyOAhaELQhaELQhaELQhaELQhaEIhaELQhaEIhDSfAhELQhaELQhaELQhaEIVT+CvaXVVsOkXrv0YEx+6K
+ZpdVGw6RfjQRZm+6KDpdUOw6Bes78WyZvuik6XVTsOiXqu7EkXs33RSdLqh2HReq3bmJWSTdz/AIKTpdKh0HR+o3alZN6STP8AdDqdLpUGg6O+3dbIskm2
+bZJJMz4HU6XSoNB0V5u67s3JJJJszPgdDpdKg0HR3p9KSbJJskmzO+B0Gl0qHQdHdbut2yTbJNs3M34HU6XSqdB0dxuFZNybkkkkkkk3c34HU6XVJ0HS3J
+m62TZJJJJJJN/N+B0Ol1SdB0tkjc3WySSSSTG9N3NHT6XVJ0HQ2N3JJGybkkkkkk3Jtzx0+l1SdB0Y3ckkbskkkkkkmySbkkkmYOh0uqtoG5RJJJNkkkkk
+kkkkkk2SSSSTbmDptLqpaISkJkjfm/FrxakWsFnCex7s35uycTwZwjAns9v6jhwxOFgMwhEs20U0bvkbs3fIpopopopopqxpokRRtHdaYLNNtrl/LiFA0S
+ked17yk2KeKyayYxMeIKCyrP3FAJjEnFJNdmLWmsSjrP69TKPNDS2xuptpTaeqsl5k8yeZPMnmTzJ5k8yeZPMj/KW1bfwJVZzmL20FAzYYuJlG0jzJ5k8y
+eZPMnmTzJ5k8yeZPMjbaW5er/BoTh/wBuxIaIhorDzK5/p+M/wAQS2UwU+RR5lHmQpaRXUZJoTnJJezWJHVEdT3nuE4dpwhrl9iRrCv5KPMq8yrzFpmkMM
+k/gxhoibTKtONuAuC+3MfEZfFum9GOeLSbsUYEuc28JuCZTJj7vwIDUnvwvAlnikxbTPMJ8CmEOLfMSZKSnUdwGJlNX54+vhJsKdxnJP0CgigigigiomVk
+yPvCPvCPvh/ZB/Zh/Yh/ch/ch/chstCXE1lHr4FUhJpPA2nIbTkNlyGy5Db8hteU2PKeCngp4KbXlNrymz5DZ8htuQ2nIYFUhpJiv8R//9k=
+"""
 
 if __name__ == '__main__':
     main()
