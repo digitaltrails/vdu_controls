dbdedc065a33ef6155b96e7931d77ec805843ece
digitaltrails <michael@actrix.gen.nz>
2023-12-21 11:20:00 +1300

    SVG formatting cleanup

--- vdu_controls.py
+++ vdu_controls.py
@@ -939,10 +939,10 @@ mono_light_tray = False
 MONOCHROME_APP_ICON = b"""
 <svg viewBox="0 0 22 22" version="1.1" id="svg1" xmlns="http://www.w3.org/2000/svg">
   <defs id="defs3051"><style type="text/css" id="current-color-scheme">.ColorScheme-Text {color:#ffffff;}</style></defs>
-  <path style="fill:currentColor;fill-opacity:1;stroke:none"
+  <path style="fill:currentColor;fill-opacity:1;stroke:none" class="ColorScheme-Text"
      d="m 3.012318,1.987629 -0.086226,13.98553 h 1 l 5.0022397,0.02464 -1e-7,2 -2.0022396,-0.02464 v 1 h 8.0000002 v -1 
      l -2.00224,-0.01232 -0.01232,-2 5.01456,0.01232 h 1 L 18.957944,2.0296853 17.989795,2.0050493 4.0174203,1.9774244 
-     Z m 0.9927843,1.0339651 13.9651597,-0.01742 -0.01954,10.9465899 -14.0000001,0.02464 z" id="rect4211" class="ColorScheme-Text"/>
+     Z m 0.9927843,1.0339651 13.9651597,-0.01742 -0.01954,10.9465899 -14.0000001,0.02464 z" id="rect4211"/>
 </svg>"""
 
 # modified brightness icon from breeze5-icons: LGPL-3.0-only
@@ -950,28 +950,19 @@ BRIGHTNESS_SVG = b"""
 <svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 24 24" width="24" height="24">
   <style type="text/css" id="current-color-scheme"> .ColorScheme-Text { color:#232629; } </style>
   <g transform="translate(1,1)">
-    <g shape-rendering="auto">
-      <path d="m11 7c-2.2032167 0-4 1.7967833-4 4 0 2.203217 1.7967833 4 4 4 2.203217 0 4-1.796783 4-4 0-2.2032167-1.796783-4-4-4zm0
-       1c1.662777 0 3 1.3372234 3 3 0 1.662777-1.337223 3-3 3-1.6627766 0-3-1.337223-3-3 0-1.6627766 1.3372234-3 3-3z" 
-       class="ColorScheme-Text" fill="currentColor"/>
-      <path d="m10.5 3v3h1v-3h-1z" class="ColorScheme-Text" fill="currentColor"/>
-      <path d="m10.5 16v3h1v-3h-1z" class="ColorScheme-Text" fill="currentColor"/>
-      <path d="m3 10.5v1h3v-1h-3z" class="ColorScheme-Text" fill="currentColor"/>
-      <path d="m16 10.5v1h3v-1h-3z" class="ColorScheme-Text" fill="currentColor"/>
-      <path d="m14.707031 14-0.707031 0.707031 2.121094 2.121094 0.707031-0.707031-2.121094-2.121094z"
-       class="ColorScheme-Text" fill="currentColor"/>
-      <path d="M 5.7070312 5 L 5 5.7070312 L 7.1210938 7.828125 L 7.828125 7.1210938 L 5.7070312 5 z "
-       class="ColorScheme-Text" fill="currentColor"/>
-      <path d="M 7.1210938 14 L 5 16.121094 L 5.7070312 16.828125 L 7.828125 14.707031 L 7.1210938 14 z "
-       class="ColorScheme-Text" fill="currentColor"/>
-      <path d="M 16.121094 5 L 14 7.1210938 L 14.707031 7.828125 L 16.828125 5.7070312 L 16.121094 5 z "
-       class="ColorScheme-Text" fill="currentColor"/>
+    <g shape-rendering="auto"  class="ColorScheme-Text" fill="currentColor">
+      <path d="m11 7c-2.2032167 0-4 1.7967833-4 4 0 2.203217 1.7967833 4 4 4 2.203217 0 4-1.796783 4-4 0-2.2032167
+       -1.796783-4-4-4zm0 1c1.662777 0 3 1.3372234 3 3 0 1.662777-1.337223 3-3 3-1.6627766 0-3-1.337223-3-3 
+       0-1.6627766 1.3372234-3 3-3z"/>  <path d="m10.5 3v3h1v-3h-1z"/> <path d="m10.5 16v3h1v-3h-1z"/> 
+      <path d="m3 10.5v1h3v-1h-3z"/> <path d="m16 10.5v1h3v-1h-3z"/>
+      <path d="m14.707031 14-0.707031 0.707031 2.121094 2.121094 0.707031-0.707031-2.121094-2.121094z"/>
+      <path d="M 5.7070312 5 L 5 5.7070312 L 7.1210938 7.828125 L 7.828125 7.1210938 L 5.7070312 5 z "/>
+      <path d="M 7.1210938 14 L 5 16.121094 L 5.7070312 16.828125 L 7.828125 14.707031 L 7.1210938 14 z "/>
+      <path d="M 16.121094 5 L 14 7.1210938 L 14.707031 7.828125 L 16.828125 5.7070312 L 16.121094 5 z "/>
       <g>
-        <path d="m11.000001 7.7500005v6.4999985h2.166665l1.083333-2.166666v-2.1666663l-1.083333-2.1666662z"
-         class="ColorScheme-Text" fill="currentColor"/>
-        <path d="m10.984375 7.734375v0.015625 6.515625h2.191406l1.089844-2.177734v-2.1757816l-1.089844-2.1777344h-2.191406zm0.03125 
-         0.03125h2.140625l1.078125 2.1542969v2.1601561l-1.078125 2.154297h-2.140625v-6.46875z"
-          class="ColorScheme-Text" fill="currentColor"/>
+        <path d="m11.000001 7.7500005v6.4999985h2.166665l1.083333-2.166666v-2.1666663l-1.083333-2.1666662z"/>
+        <path d="m10.984375 7.734375v0.015625 6.515625h2.191406l1.089844-2.177734v-2.1757816l-1.089844-2.1777344h
+         -2.191406zm0.03125 0.03125h2.140625l1.078125 2.1542969v2.1601561l-1.078125 2.154297h-2.140625v-6.46875z"/>
       </g>
     </g>
   </g>
@@ -983,9 +974,9 @@ CONTRAST_SVG = b"""
 <svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 24 24" width="24" height="24">
   <style type="text/css" id="current-color-scheme"> .ColorScheme-Text { color:#232629; } </style>
   <g transform="translate(1,1)">
-    <path style="fill:currentColor;fill-opacity:1;stroke:none" transform="translate(-1,-1)" d="m 12,7 c -2.761424,0 -5,2.2386
-     -5,5 0,2.7614 2.238576,5 5,5 2.761424,0 5,-2.2386 5,-5 0,-2.7614 -2.238576,-5 -5,-5 z m 0,1 v 8 C 9.790861,16 8,14.2091 8,12
-      8,9.7909 9.790861,8 12,8" class="ColorScheme-Text" id="path79" />
+    <path transform="translate(-1,-1)" class="ColorScheme-Text" style="fill:currentColor;fill-opacity:1;stroke:none" 
+      d="m 12,7 c -2.761424,0 -5,2.2386 -5,5 0,2.7614 2.238576,5 5,5 2.761424,0 5,-2.2386 5,-5 0,-2.7614 
+      -2.238576,-5 -5,-5 z m 0,1 v 8 C 9.790861,16 8,14.2091 8,12 8,9.7909 9.790861,8 12,8"  id="path79" />
   </g>
 </svg>
 """
@@ -1018,10 +1009,10 @@ VOLUME_SVG = b"""
   <style type="text/css" id="current-color-scheme"> .ColorScheme-Text { color:#232629; } </style>
   <g transform="translate(1,1)">
     <g class="ColorScheme-Text" fill="currentColor">
-      <path d="m14.324219 7.28125-.539063.8613281a4 4 0 0 1 1.214844 2.8574219 4 4 0 0 1 -1.210938 2.861328l.539063.863281a5
-       5 0 0 0 1.671875-3.724609 5 5 0 0 0 -1.675781-3.71875z"/>
-      <path d="m13.865234 3.5371094-.24414.9765625a7 7 0 0 1 4.378906 6.4863281 7 7 0 0 1 -4.380859 6.478516l.24414.974609a8
-       8 0 0 0 5.136719-7.453125 8 8 0 0 0 -5.134766-7.4628906z"/>
+      <path d="m14.324219 7.28125-.539063.8613281a4 4 0 0 1 1.214844 2.8574219 4 4 0 0 1 -1.210938 
+       2.861328l.539063.863281a5 5 0 0 0 1.671875-3.724609 5 5 0 0 0 -1.675781-3.71875z"/>
+      <path d="m13.865234 3.5371094-.24414.9765625a7 7 0 0 1 4.378906 6.4863281 7 7 0 0 1 -4.380859 
+       6.478516l.24414.974609a8 8 0 0 0 5.136719-7.453125 8 8 0 0 0 -5.134766-7.4628906z"/>
       <path d="m3 8h2v6h-2z" fill-rule="evenodd"/>
       <path d="m6 14 5 5h1v-16h-1l-5 5z"/>
     </g>
@@ -1056,14 +1047,8 @@ REFRESH_ICON_SOURCE = b"""
 
 LIGHTING_CHECK_SVG = b"""
 <svg version="1.1" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
-    <defs>
-        <style id="current-color-scheme" type="text/css">.ColorScheme-Text {
-        color:#232629;
-      }
-      .ColorScheme-NegativeText {
-          color:#ff8500;
-      }</style>
-    </defs>
+    <defs> <style id="current-color-scheme" type="text/css">.ColorScheme-Text { color:#232629; }
+      .ColorScheme-NegativeText { color:#ff8500; }</style> </defs>
     <path style="fill:currentColor;fill-opacity:1;stroke:none" class="ColorScheme-Text" d="M 5,3 V 4 H 7 V 5.0507812
      C 4.7620407,5.3045267 3,7.1975144 3,9.5 3,11.813856 4.7794406,13.714649 7.0332031,13.953125 6.6992186,13.613635 
      6.43803,13.209557 6.265625,12.765625 4.9435886,12.265608 4,10.997158 4,9.5 4,7.5670034 5.5670034,6 7.5,6 
@@ -6822,11 +6807,9 @@ LUX_NIGHT_SVG = b"""<?xml version="1.0" encoding="utf-8"?>
 <svg width="24px" height="24px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
     <style type="text/css" id="current-color-scheme"> .ColorScheme-Text { color:#232629; } </style>
     <g class="ColorScheme-Text" stroke="none" stroke-linecap="square" stroke-width="0.5" fill="currentColor">
-        <path d="M 8,5 m2.5,.1 .6,1.7h1.8l-1.4,1.1 .5,1.7-1.5-1.0-1.5,1.0 .5-1.7-1.4-1.1h1.8z"
-            transform="scale(1.0)"/>
-        <path d="M 4,8 m2.5,.1 .6,1.7h1.8l-1.4,1.1 .5,1.7-1.5-1.0-1.5,1.0 .5-1.7-1.4-1.1h1.8z"
-            transform="scale(1.2)"/>
-        <path d="M 10,13 m2.5,.1 .6,1.7h1.8l-1.4,1.1 .5,1.7-1.5-1.0-1.5,1.0 .5-1.7-1.4-1.1h1.8z"
+        <path d="M 8,5 m2.5,.1 .6,1.7h1.8l-1.4,1.1 .5,1.7-1.5-1.0-1.5,1.0 .5-1.7-1.4-1.1h1.8z" transform="scale(1.0)"/>
+        <path d="M 4,8 m2.5,.1 .6,1.7h1.8l-1.4,1.1 .5,1.7-1.5-1.0-1.5,1.0 .5-1.7-1.4-1.1h1.8z" transform="scale(1.2)"/>
+        <path d="M 10,13 m2.5,.1 .6,1.7h1.8l-1.4,1.1 .5,1.7-1.5-1.0-1.5,1.0 .5-1.7-1.4-1.1h1.8z" 
             transform="scale(1.1)"/>
         <path d="M 12.5,8.5 m2.5,.1 .6,1.7h1.8l-1.4,1.1 .5,1.7-1.5-1.0-1.5,1.0 .5-1.7-1.4-1.1h1.8z"
             transform="scale(1.0)"/>
