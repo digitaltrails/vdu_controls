379e06f25ea7119cd077037af4d6613cb0a5acc0
digitaltrails <michael@actrix.gen.nz>
2025-02-18 20:18:56 +1300

    Update for 2.1.3

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ copyright = '2021, Michael Hamilton'
 author = 'Michael Hamilton'
 
 # The full version, including alpha/beta/rc tags
-release = '2.1.2'
+release = '2.1.3'
 
 
 # -- General configuration ---------------------------------------------------
