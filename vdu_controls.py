b9bdbec6dfd0b7bba301ef3d26dbf08bc1d005fa
digitaltrails <michael@actrix.gen.nz>
2025-05-01 14:40:29 +1200

    Bump version to 2.3.1

--- vdu_controls.py
+++ vdu_controls.py
@@ -862,7 +862,7 @@ from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QSl
     QDesktopWidget, QSpacerItem
 
 APPNAME = "VDU Controls"
-VDU_CONTROLS_VERSION = '2.3.0'
+VDU_CONTROLS_VERSION = '2.3.1'
 VDU_CONTROLS_VERSION_TUPLE = tuple(int(i) for i in VDU_CONTROLS_VERSION.split('.'))
 assert sys.version_info >= (3, 8), f'{APPNAME} utilises python version 3.8 or greater (your python is {sys.version}).'
 
