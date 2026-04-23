9108d89b4c4f74db7029ac6b30fb349fadc9a93c
digitaltrails <michael@actrix.gen.nz>
2026-04-23 14:45:12 +1200

    Put back a main() for zipapp to use.

--- vdu_controls/vdu_controls_main.py
+++ vdu_controls/vdu_controls_main.py
@@ -1,6 +1,10 @@
 import sys
 
-if __name__ == '__main__':
-    import vdu_controls.vdu_controls_application as application
+import vdu_controls.vdu_controls_application as application
+
+def main():
     application.main()
+
+if __name__ == '__main__':
+    main()
     sys.exit(0)
