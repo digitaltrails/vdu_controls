95805404b01d592332cd7a16e8f281769cc59ef8
digitaltrails <michael@actrix.gen.nz>
2026-06-28 20:00:51 +1200

    Release note fixes.

--- src/vdu_controls/release.py
+++ src/vdu_controls/release.py
@@ -11,27 +11,32 @@ from vdu_controls.svg import VDU_CONTROLS_ICON_SVG
 from vdu_controls.widgets import MBox, MIcon, MBtn
 
 class Release:
-    RELEASE_WELCOME = QT_TR_NOOP("Welcome to vdu_controls {}").format(VDU_CONTROLS_VERSION)
+    RELEASE_WELCOME = QT_TR_NOOP("Welcome to vdu_controls {}")
     RELEASE_NOTE = QT_TR_NOOP("Please read the online release notes:")
     RELEASE_ANNOUNCEMENT = """<h3>{WELCOME}</h3>{NOTE}<br/>
     <a href="https://github.com/digitaltrails/vdu_controls/releases/tag/v{VERSION}">
     https://github.com/digitaltrails/vdu_controls/releases/tag/v{VERSION}</a>
-    <br/>___________________________________________________________________________"""
+    <br/>_____________________________________________________________________________________________"""
     RELEASE_INFO = QT_TR_NOOP(
-                              '<b><span style="color: #feae0f;">\u26A0</span> Important notice:</b>'
-                              ' the <i>weather feature</i> may be removed from the next version.</b>'
+                              '<h3><span style="color: #ea4335;">\u26A0</span> Proposed Changes - feedback required</h3>'
+                              'You may wish to comment on these proposed changes:'
+                              ' <ul><li>The <i>weather feature</i> may be removed from the next version.</b>'
                               ' If you rely on it, please comment on issue'
-                              ' <a href="https://github.com/digitaltrails/vdu_controls/issues/115">#155</a>.'
-                              '<br/><br/>'
-                              "<b><span style='color: #2196F3;'>\U0001F6C8</span> What's new:</b> usability improvements and code refactoring. "
-                              'See the online release notes for details.'
+                              ' <a href="https://github.com/digitaltrails/vdu_controls/issues/133">#133</a>.'
+                              ' <li>The <i>protect-nvram feature</i> has been made mandatory.</b>'
+                              ' If you rely on disabling protect-nvram, please comment on issue'
+                              ' <a href="https://github.com/digitaltrails/vdu_controls/issues/132">#132</a>.'
+                              '</ul>'
+                              "<h3><span style='color: #2196F3;'>\U0001F6C8</span> What's new:</h3> Usability improvements and code refactoring, "
+                              'see the <a href="https://github.com/digitaltrails/vdu_controls/releases/tag/v{VERSION}">'
+                              'online release notes</a> for details.'
                               '<br/><br/>'
                               )
     @staticmethod
     def release_notes():
         release_alert = MBox(
             MIcon.Information,
-            msg=Release.RELEASE_ANNOUNCEMENT.format(WELCOME=tr(Release.RELEASE_WELCOME, Release.__name__),
+            msg=Release.RELEASE_ANNOUNCEMENT.format(WELCOME=tr(Release.RELEASE_WELCOME, Release.__name__).format(VDU_CONTROLS_VERSION),
                                                     NOTE=tr(Release.RELEASE_NOTE, Release.__name__),
                                                     VERSION=VDU_CONTROLS_VERSION),
             info=tr(Release.RELEASE_INFO, Release.__name__), buttons=MBtn.Close)
