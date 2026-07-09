a9d47f4c430bbe8b08911cbc0570430a93827040
digitaltrails <michael@actrix.gen.nz>
2026-07-10 11:22:19 +1200

    Update for module

--- src/vdu_controls/__init__.py
+++ src/vdu_controls/__init__.py
@@ -1,2 +1,7 @@
 # SPDX-FileCopyrightText: 2021-2026 Contributors to vdu_controls <https://github.com/digitaltrails/vdu_controls>
 # SPDX-License-Identifier: GPL-3.0-or-later
+"""vdu_controls – Visual Display Unit virtual control panel."""
+
+from .vdu_controls_application import main
+
+__all__ = ["main"]
